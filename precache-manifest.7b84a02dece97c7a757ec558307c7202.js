2e2b6d0cfd6da987b783f17520a6db60455aff95
kqarlos <kqarlos@hotmail.com>
2020-02-09 10:33:45 -0800

    Updates

---
+++ precache-manifest.7b84a02dece97c7a757ec558307c7202.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1b1b62b671e2d7749e9310e761c7480",
+    "url": "/memory-game/index.html"
+  },
+  {
+    "revision": "03632ef55a165361c87b",
+    "url": "/memory-game/static/css/main.3ccf3e8b.chunk.css"
+  },
+  {
+    "revision": "73af865cf4fe0aa15360",
+    "url": "/memory-game/static/js/2.35944eca.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/memory-game/static/js/2.35944eca.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "03632ef55a165361c87b",
+    "url": "/memory-game/static/js/main.f9634a63.chunk.js"
+  },
+  {
+    "revision": "7ba82378826073dce3ea",
+    "url": "/memory-game/static/js/runtime-main.02752f75.js"
+  }
+]);
